bc065fbf84913d57ae31cc39a9e8247a3a4674f5
EbotProg <achaleebotoma2002@gmail.com>
2025-03-11 12:39:28 +0100

    removed anchor

--- www/index.php
+++ www/index.php
@@ -53,7 +53,6 @@
                         <div class="card-body">
                             <h4 class="card-title">Card title</h4>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
                         </div>
                     </div>
                 </div>
